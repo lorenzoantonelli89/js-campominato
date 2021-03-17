e124bc76f41cf0fbfa8b2d968e6caf99abff3039
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-17 18:06:50 +0100

    update inserito bombe

--- superbonus/js/first.js
+++ superbonus/js/first.js
@@ -1,105 +1,47 @@
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati.
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 => tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
 
-// var result = document.getElementById('result');
-//
-// var numMathRnd = 100;
-// var valueUtente = 84;
-//
-// var difficultBtn = document.getElementById('difficult');
-//
-// difficultBtn.addEventListener('change', function(){
-//   var difficult = document.getElementById('difficult').value;
-//
-//   if (difficult == 1) {
-//      numMathRnd = 100;
-//      valueUtente = 84;
-//   }else if (difficult == 2) {
-//      numMathRnd = 80;
-//      valueUtente = 64;
-//   }else if (difficult == 3) {
-//     numMathRnd = 50;
-//     valueUtente = 34;
-//   }
-//
-// });
-//
-// function numPc(){
-//   arrNumRnd = [];
-//
-//   while (arrNumRnd.length < 16) {
-//     var rnd = Math.floor(Math.random() * numMathRnd) + 1;
-//     if (!arrNumRnd.includes(rnd)) {
-//       arrNumRnd.push(rnd);
-//     }
-//   }
-//
-//   console.log(arrNumRnd);
-// }
-//
-//
-// function numPlayer(){
-//
-//   numUtente = [];
-//
-//   while (numUtente.length < valueUtente) {
-//     var numInsert = parseInt(prompt('Scrivi un numero compreso tra 1 e ' + numMathRnd));
-//     if (numUtente.includes(numInsert)) {
-//       alert('Hai inserito un numero doppio');
-//       continue
-//     }
-//     numUtente.push(numInsert);
-//
-//     if (arrNumRnd.includes(numInsert)) {
-//       result.innerHTML = 'Hai perso ma hai totalizzato: ' + (numUtente.length - 1) + ' punti';
-//       console.log('Hai perso ma hai totalizzato: ' + (numUtente.length - 1) + ' punti');
-//       break;
-//     }else if (numUtente.length == valueUtente) {
-//       result.innerHTML = 'Hai vinto';
-//       console.log('Hai vinto');
-//     }
-//
-//   }
-// }
-//
-// function campominato(){
-//   numPc();
-//   numPlayer();
-// }
-//
-//
-// var playBtn = document.getElementById('play');
-//
-// playBtn.addEventListener('click', function(){
-//
-//   campominato();
-//
-// });
-
-// var tdBtn = document.getElementById('test');
-//
-// tdBtn.addEventListener('click', function(){
-//   console.log('test');
-// });
-
-function generaBomb() {
-  for (var i = 0; i < bomb; i++) {
-    var rnd = Math.floor(Math.random() * bomb) + 1;
-    console.log(rnd);
+function onClick(){
+  var clickedTd = this;
+  var clickedTdValue = parseInt(clickedTd.dataset.number);
+  var square = clickedTd.children[0];
+  var bombFound = clickedTd.children[1];
+  console.log(clickedTdValue, bombRnd);
+  console.log(square);
+  if (bombRnd.includes(clickedTdValue)) {
+    square.style.display = "none";
+    bombFound.style.display = "block";
+  }else {
+    square.style.display = "none";
   }
+
+  clickedTd.removeEventListener('click', onClick);
+  console.log(clickedTdValue);
+
 }
 
-var bomb = 0;
+var tdBtn = document.getElementsByClassName("table-cell");
+
+
+for (var i = 0; i < tdBtn.length; i++) {
+  var elemTd = tdBtn[i];
+  elemTd.addEventListener('click', onClick);
+
+}
+
+var bombRnd = [];
+
+function generaBomb() {
+  while (bombRnd.length < bomb) {
+    var rnd = Math.floor(Math.random() * 100) + 1;
+    if (!bombRnd.includes(rnd)) {
+      bombRnd.push(rnd);
+    }
+
+    console.log(rnd);
+  }
+
+}
+
+var bomb = 15;
 
 function selectDifficult() {
 
@@ -125,24 +67,3 @@ function selectDifficult() {
     }
 }
 selectDifficult();
-
-
-
-
-var tdBtn = document.getElementsByClassName("table-cell");
-
-
-for (var i = 0; i < tdBtn.length; i++) {
-  var elemTd = tdBtn[i];
-  elemTd.addEventListener('click', function(){
-    var clickedTd = this;
-    var clickedTdValue = clickedTd.dataset.number;
-    console.log(clickedTdValue);
-
-
-  });
-
-}
-
-// modificare al click
-// this.innerHTML = this.dataset.number;
